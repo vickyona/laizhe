9eccb514fcd242c853b18c6cda836030db1c0e9e
fanwei <fanweib4w@163.com>
2017-11-03 09:59:33 +0800

    change

--- src/pages/home/Swiper.vue
+++ src/pages/home/Swiper.vue
@@ -1,39 +1,28 @@
 <template>
- <swiper :options="swiperOption" ref="mySwiper" id="swiper">
-    <!-- slides -->
+ <swiper :options="swiperOption" id="swiper" ref="mySwiper">
       <swiper-slide><img src="../../assets/swipers/swiper1.jpg" alt=""></swiper-slide>
       <swiper-slide><img src="../../assets/swipers/swiper2.png" alt=""></swiper-slide>
       <swiper-slide><img src="../../assets/swipers/swiper3.jpg" alt=""></swiper-slide>
       <swiper-slide><img src="../../assets/swipers/swiper4.jpg" alt=""></swiper-slide>
       <swiper-slide><img src="../../assets/swipers/swiper5.jpg" alt=""></swiper-slide>
       <swiper-slide><img src="../../assets/swipers/swiper6.jpg" alt=""></swiper-slide>
-    <!-- Optional controls -->
       <div class="swiper-pagination"  slot="pagination"></div>
-      <div class="swiper-scrollbar"   slot="scrollbar"></div>
   </swiper>
 </template>
 
 <script>
 import { swiper, swiperSlide } from "vue-awesome-swiper";
 export default {
-  name: "carrousel",
   data() {
     return {
       swiperOption: {
-        notNextTick: false,
         autoplay: 3000,
-        centeredSlides: true,
-        // direction : 'vertical',
+        direction: "horizontal",
         spaceBetween: 0,
         effect: "coverflow",
-        grabCursor: true,
-        setWrapperSize: true,
         autoHeight: true,
-        paginationType: "dots",
+        paginationType: "bullets",
         pagination: ".swiper-pagination",
-        paginationClickable: true,
-        scrollbar: ".swiper-scrollbar",
-        mousewheelControl: true,
         observeParents: true
       }
     };
@@ -41,11 +30,6 @@ export default {
   components: {
     swiper,
     swiperSlide
-  },
-  computed: {
-    swiper() {
-      return this.$refs.mySwiper.swiper;
-    }
   }
 };
 </script>
