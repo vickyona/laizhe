70a15d5e27ae057d7e07695f94b66f4568b116bb
weiyu1069 <13210693473@163.com>
2017-11-04 14:30:38 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,26 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import Swiper from '@/pages/home/Swiper'
-
+import City from '@/pages/city/City'
+import Search from '@/pages/search/Search'
+import Order from '@/pages/order/Order'
 Vue.use(Router)
 export default new Router({
   routes: [{
     path: '/',
     name: 'Home',
     component: Home
-  }, {
-    path: '/',
-    name: '轮播',
-    component: Swiper,
+  },{
+    path: '/city',
+    name: 'City',
+    component: City,
+  },{
+    path: '/search',
+    name: 'Search',
+    component: Search,
+  },{
+    path: '/order',
+    name: 'Order',
+    component: Order,
   }]
 })
