c9d7ca46da2bf315d7bf3932ccbfa9334bbfbebb
ZhendongZhang666 <18295793369@163.com>
2017-11-04 13:33:37 +0800

    city

---
+++ src/pages/city/City.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+ 		
+ 	</div>
+</template>
+
+<script>
+import HeaderComponent from "./Header";
+import SwiperContent from "./Swiper";
+import NavBoard from "./NavBoard";
+import OnSale from "./OnSale";
+import HotPlace from "./HotPlace";
+export default {
+  components: {
+    "home-header": HeaderComponent,
+    "swiper-content": SwiperContent,
+    "navBoard-content": NavBoard,
+    "onSale-content": OnSale,
+    "hotPlace-content": HotPlace
+  }
+};
+</script>
+
+<style>
+html {
+  background: #f5f5f5;
+}
+</style>
