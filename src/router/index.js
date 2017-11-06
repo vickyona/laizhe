39c304057b4ab39a861cd31885c96841704ca32d
fanwei <fanweib4w@163.com>
2017-11-06 11:06:48 +0800

    change

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import Swiper from '@/pages/home/Swiper'
-import Order from '@/pages/order/Order'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/pages/home/Home';
+import Swiper from '@/pages/home/Swiper';
+import Order from '@/pages/order/Order';
 
 Vue.use(Router)
 export default new Router({
