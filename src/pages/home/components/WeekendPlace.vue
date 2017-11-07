54b7244917bbceef708dfc8285c16ea13f32e553
fanwei <fanweib4w@163.com>
2017-11-07 10:31:06 +0800

    addInfo

--- src/pages/home/components/WeekendPlace.vue
+++ src/pages/home/components/WeekendPlace.vue
@@ -5,10 +5,10 @@
           <div class="mp-product-item" v-for="item in weekInfo" :key="item.title">
             <a  :title="item.title" class="mp-fulllink">
               <div class="product-imgcontainer image-ready">
-                <img :src="item.path" :alt="item.title" class="title-image">
+                <img :src="item.path" :alt="item.name" class="title-image">
               </div>
               <div class="mp-product-info">
-                <p class="product-name">{{item.title}}</p>
+                <p class="product-name">{{item.name}}</p>
                 <p class="product-desc">{{item.slogan}}</p>
               </div>
             </a>
@@ -23,27 +23,27 @@ export default {
       weekInfo: [
         {
           path: require("../../../assets/weekend/week1.jpg"),
-          title: "北京温泉排行榜",
+          name: "北京温泉排行榜",
           slogan: "细数北京温泉，温暖你的冬天"
         },
         {
           path: require("../../../assets/weekend/week2.jpg"),
-          title: "寻找北京的皇城范儿",
+          name: "寻找北京的皇城范儿",
           slogan: "数百年的宫廷庙宇，至今依旧威严霸气"
         },
         {
           path: require("../../../assets/weekend/week3.jpg"),
-          title: "北京必游TOP10",
+          name: "北京必游TOP10",
           slogan: "来北京必去的景点非这些地方莫属"
         },
         {
           path: require("../../../assets/weekend/week4.jpg"),
-          title: "登高望远秋色佳",
+          name: "登高望远秋色佳",
           slogan: "山顶满眼彩色，此情此景醉了"
         },
         {
           path: require("../../../assets/weekend/week5.jpg"),
-          title: "秋高气爽去礼佛",
+          name: "秋高气爽去礼佛",
           slogan: "秋风伴着美妙经声，品尝秋的另一番滋味"
         }
       ]
