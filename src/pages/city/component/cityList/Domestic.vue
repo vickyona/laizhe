3e4f0e7bfffeed1412352784f567084ac02a10f1
fanwei <fanweib4w@163.com>
2017-11-08 18:18:15 +0800

    change

--- src/pages/city/component/cityList/Domestic.vue
+++ src/pages/city/component/cityList/Domestic.vue
@@ -1,7 +1,7 @@
 <template>
  	<div id="city">
     <div class="header-keyword">
-      <input type="text" placeholder="请输入城市名或拼音请输入城市名或拼音请输入城市名或拼音请输入城市名或拼音请输入城市名或拼音" class="city-keyword" />
+      <input type="text" placeholder="请输入城市名或拼音" class="city-keyword" />
     </div>
     <div class="city-group">
       <div class="city-group-title">您的位置</div>
@@ -22,8 +22,7 @@
             </div> 
           </div>
       </div>
-      <!-- 22222 -->
-      <div class="city-hot" v-for="(items, index) in cities" v-bind:id="index">
+      <div class="city-hot" v-for="(items, index) in cities" :id="index">
           <div class="city-group-title">{{index}}</div>
           <div class="city-center" v-for="item in items">
             <a href="" class="city-normal" >{{item}}</a>
@@ -31,15 +30,20 @@
       </div>
     </div>
    
-    <ul class="right-sidebar" id="position" >
-      <li v-for="(items, index) in cities" class="city-letter" :key="index" @touchstart="handleTouchstart" @touchmove="handleTouchstart">{{index}}</li>
+    <ul class="right-sidebar" id="position" @touchmove="handleTouchmove" @touchstart="handleTouchstart">
+      <li v-for="(items, index) in cities" class="city-letter" :key="index">{{index}}</li>
     </ul>
   </div>
 </template>
 
 <script>
-import {mapState} from "vuex"
+import { mapState } from "vuex";
 export default {
+  data() {
+    return {
+      cityList: []
+    };
+  },
   computed: mapState({
     positionCity(state) {
       return state.city.domestic.positionCity;
@@ -48,16 +52,28 @@ export default {
       return state.city.domestic.hotCity;
     },
     cities(state) {
+      this.cityList = state.city.domestic.cities;
       return state.city.domestic.cities;
     }
   }),
   methods: {
-    handleTouchstart: function(e) {
+    handleTouchmove(e) {
+      e = event || window.event;
+      const touchedCitysNum =
+        ((e.changedTouches[0].clientY - 108) / 16).toFixed(0) - 1;
+      var arr = [];
+      for (let i in this.cityList) {
+        arr.push(i);
+      }
+      const positionElement = document.getElementById(arr[touchedCitysNum]);
+      document.documentElement.scrollTop = positionElement.offsetTop - 44;
+      e.preventDefault();
+    },
+    handleTouchstart(e) {
       var liElement = e.target;
       var text = liElement.innerHTML;
       var positionElement = document.getElementById(text);
-      
-      if(positionElement){
+      if (positionElement) {
         document.documentElement.scrollTop = positionElement.offsetTop - 44;
       }
     }
@@ -67,27 +83,27 @@ export default {
 
 <style>
 .header-keyword {
-  margin-top: .88rem;
-  padding: 0 .2rem .1rem .2rem ;
+  margin-top: 0.88rem;
+  padding: 0 0.2rem 0.1rem 0.2rem;
   background: #00afc7;
-  font-size: .26rem;
+  font-size: 0.26rem;
   color: #9e9e9e;
 }
 .city-keyword {
   width: 100%;
-  height: .3rem;
-  line-height: .3rem;
-  padding: .16rem 0 .16rem .1rem;
+  height: 0.3rem;
+  line-height: 0.3rem;
+  padding: 0.16rem 0 0.16rem 0.1rem;
   background: #fff;
   border: 0;
-  border-radius: .06rem;
+  border-radius: 0.06rem;
   text-align: center;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
 }
 .city-position {
-  font-size: .28rem;
+  font-size: 0.28rem;
 }
 .city-group {
   width: 100%;
@@ -95,10 +111,10 @@ export default {
 .city-group-title {
   width: 100%;
   box-sizing: border-box;
-  line-height: .54rem;
-  padding: 0 .3rem;
+  line-height: 0.54rem;
+  padding: 0 0.3rem;
   color: #616161;
-  font-size: .26rem;
+  font-size: 0.26rem;
   border-bottom: 1px solid #ccc;
   overflow: hidden;
   white-space: nowrap;
@@ -107,8 +123,8 @@ export default {
 .city-group-now {
   width: 100%;
   box-sizing: border-box;
-  overflow: hidden; 
-  padding: .04rem .5rem .26rem .2rem;
+  overflow: hidden;
+  padding: 0.04rem 0.5rem 0.26rem 0.2rem;
   background: #fff;
   border-bottom: 1px solid #ccc;
 }
@@ -116,18 +132,18 @@ export default {
   box-sizing: border-box;
   float: left;
   width: 33.33%;
-  height: .6rem;
-  line-height: .6rem;
+  height: 0.6rem;
+  line-height: 0.6rem;
   overflow: hidden;
-  padding: .15rem .14rem .65rem .1rem;
+  padding: 0.15rem 0.14rem 0.65rem 0.1rem;
 }
 
 .city-name {
   display: block;
   line-height: 30px;
   text-align: center;
-  border: .02rem solid #c9cccd;
-  border-radius: .06rem;
+  border: 0.02rem solid #c9cccd;
+  border-radius: 0.06rem;
   color: #212121;
   overflow: hidden;
   white-space: nowrap;
@@ -141,19 +157,19 @@ export default {
   display: block;
   width: 100%;
   box-sizing: border-box;
-  line-height: .76rem;
-  padding: 0 .2rem;
-  font-size: .28rem;
+  line-height: 0.76rem;
+  padding: 0 0.2rem;
+  font-size: 0.28rem;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
   color: #212121;
-  border-bottom: .02rem solid #ccc;
+  border-bottom: 0.02rem solid #ccc;
 }
 .city-center {
   width: 100%;
   box-sizing: border-box;
-  overflow: hidden; 
+  overflow: hidden;
   background: #fff;
 }
 .right-sidebar {
@@ -162,12 +178,12 @@ export default {
   right: 0rem;
 }
 .city-letter {
-  width: .32rem;
-  height: .32rem;
-  line-height: .32rem;
-  padding-left: .2rem;
+  width: 0.32rem;
+  height: 0.32rem;
+  line-height: 0.32rem;
+  padding-left: 0.2rem;
   color: #00afc7;
-  font-size: .24rem;
+  font-size: 0.24rem;
   text-align: center;
   overflow: hidden;
 }
--- src/pages/city/component/cityList/Foreign.vue
+++ src/pages/city/component/cityList/Foreign.vue
@@ -1,7 +1,7 @@
 <template>
  	<div id="city">
     <div class="header-keyword">
-      <input type="text" placeholder="请输入城市名或拼音请输入城市名或拼音请输入城市名或拼音请输入城市名或拼音请输入城市名或拼音" class="city-keyword" />
+      <input type="text" placeholder="请输入城市名或拼音" class="city-keyword" />
     </div>
     <div class="city-group">
       <div class="city-group-title">您的位置</div>
@@ -32,15 +32,15 @@
     </div>
    
     <ul class="right-sidebar" id="position">
-      <li v-for="(items, index) in cities" class="city-letter" :key="index" @touchstart="handleTouchstart">{{index}}</li>
+      <li v-for="(items, index) in cities" class="city-letter" :key="index" @touchstart="handleTouchstart" @touchmove="handleTouchmove">{{index}}</li>
     </ul>
   </div>
 </template>
 
 <script>
-import {mapState} from "vuex"
+import { mapState } from "vuex";
 export default {
-   computed: mapState({
+  computed: mapState({
     positionCity(state) {
       return state.city.foreign.positionCity;
     },
@@ -56,10 +56,22 @@ export default {
       var liElement = e.target;
       var text = liElement.innerHTML;
       var positionElement = document.getElementById(text);
-      
-      if(positionElement){
+
+      if (positionElement) {
         document.documentElement.scrollTop = positionElement.offsetTop - 44;
       }
+    },
+    handleTouchmove(e) {
+      e = event || window.event;
+      let touchedCitysNum =
+        ((e.changedTouches[0].clientY - 108) / 16).toFixed(0) - 1;
+      var arr = [];
+      for (let i in this.cityList) {
+        arr.push(i);
+      }
+      let positionElement = document.getElementById(arr[touchedCitysNum]);
+      document.documentElement.scrollTop = positionElement.offsetTop - 44;
+      e.preventDefault();
     }
   }
 };
@@ -67,27 +79,27 @@ export default {
 
 <style>
 .header-keyword {
-  margin-top: .88rem;
-  padding: 0 .2rem .1rem .2rem ;
+  margin-top: 0.88rem;
+  padding: 0 0.2rem 0.1rem 0.2rem;
   background: #00afc7;
-  font-size: .26rem;
+  font-size: 0.26rem;
   color: #9e9e9e;
 }
 .city-keyword {
   width: 100%;
-  height: .3rem;
-  line-height: .3rem;
-  padding: .16rem 0 .16rem .1rem;
+  height: 0.3rem;
+  line-height: 0.3rem;
+  padding: 0.16rem 0 0.16rem 0.1rem;
   background: #fff;
   border: 0;
-  border-radius: .06rem;
+  border-radius: 0.06rem;
   text-align: center;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
 }
 .city-position {
-  font-size: .28rem;
+  font-size: 0.28rem;
 }
 .city-group {
   width: 100%;
@@ -95,10 +107,10 @@ export default {
 .city-group-title {
   width: 100%;
   box-sizing: border-box;
-  line-height: .54rem;
-  padding: 0 .3rem;
+  line-height: 0.54rem;
+  padding: 0 0.3rem;
   color: #616161;
-  font-size: .26rem;
+  font-size: 0.26rem;
   border-bottom: 1px solid #ccc;
   overflow: hidden;
   white-space: nowrap;
@@ -107,8 +119,8 @@ export default {
 .city-group-now {
   width: 100%;
   box-sizing: border-box;
-  overflow: hidden; 
-  padding: .04rem .5rem .26rem .2rem;
+  overflow: hidden;
+  padding: 0.04rem 0.5rem 0.26rem 0.2rem;
   background: #fff;
   border-bottom: 1px solid #ccc;
 }
@@ -116,18 +128,18 @@ export default {
   box-sizing: border-box;
   float: left;
   width: 33.33%;
-  height: .6rem;
-  line-height: .6rem;
+  height: 0.6rem;
+  line-height: 0.6rem;
   overflow: hidden;
-  padding: .15rem .14rem .65rem .1rem;
+  padding: 0.15rem 0.14rem 0.65rem 0.1rem;
 }
 
 .city-name {
   display: block;
   line-height: 30px;
   text-align: center;
-  border: .02rem solid #c9cccd;
-  border-radius: .06rem;
+  border: 0.02rem solid #c9cccd;
+  border-radius: 0.06rem;
   color: #212121;
   overflow: hidden;
   white-space: nowrap;
@@ -141,19 +153,19 @@ export default {
   display: block;
   width: 100%;
   box-sizing: border-box;
-  line-height: .76rem;
-  padding: 0 .2rem;
-  font-size: .28rem;
+  line-height: 0.76rem;
+  padding: 0 0.2rem;
+  font-size: 0.28rem;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
   color: #212121;
-  border-bottom: .02rem solid #ccc;
+  border-bottom: 0.02rem solid #ccc;
 }
 .city-center {
   width: 100%;
   box-sizing: border-box;
-  overflow: hidden; 
+  overflow: hidden;
   background: #fff;
 }
 .right-sidebar {
@@ -162,12 +174,12 @@ export default {
   right: 0rem;
 }
 .city-letter {
-  width: .32rem;
-  height: .32rem;
-  line-height: .32rem;
-  padding-left: .2rem;
+  width: 0.32rem;
+  height: 0.32rem;
+  line-height: 0.32rem;
+  padding-left: 0.2rem;
   color: #00afc7;
-  font-size: .24rem;
+  font-size: 0.24rem;
   text-align: center;
   overflow: hidden;
 }
